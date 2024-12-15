b4b1e67ff1cc7c02baf1590f7c6a518df195056b
Jenny <jenny.c.soderberg@gmail.com>
2024-12-15 14:57:56 +0100

    Clean up Day 2

--- AdventOfCode/src/Day2.kt
+++ AdventOfCode/src/Day2.kt
@@ -4,17 +4,17 @@ import kotlin.math.sign
 class Day2:Day() {
     override fun RunPartTwo(input: String): String
     {
-        return RunDay(input, true)
+        return runDay(input, true)
     }
 
     override fun RunPartOne(input: String): String
     {
-        return RunDay(input, false)
+        return runDay(input, false)
     }
 
-    fun CheckLineSafety(line: String): Boolean
+    private fun checkLineSafety(line: String): Boolean
     {
-        var currentLine = line.replace(Regex("\\s+"), " ")
+        val currentLine = line.replace(Regex("\\s+"), " ")
         val levels = currentLine.split(" ")
         var previousLevel: Int? = null
         var safe: Boolean = true
@@ -49,42 +49,43 @@ class Day2:Day() {
         return safe
     }
 
-    fun RunDay(input: String, allowOneOff: Boolean = false):String
+    private fun runDay(input: String, allowOneOff: Boolean = false):String
     {
         val lines = SplitLines(input)
         var numberOfSafeReports = 0
         for(line in lines)
         {
-            if(line.trim().length == 0) continue
-
+            if(line.trim().isEmpty()) continue
             val currentLine = line.trim()
-            var safe = CheckLineSafety(currentLine)
+
+            var safe = checkLineSafety(currentLine)
             if(!safe && allowOneOff)
             {
-                val levelArray: Array<Int> = currentLine.split("\\s+".toRegex()).map{it.toInt()}.toTypedArray()
-                for(i in levelArray.indices)
-                {
-                    var shortLine: String = ""
-                    for(levelIndex in levelArray.indices)
-                    {
-                        if(i == levelIndex) continue
-                        shortLine += levelArray[levelIndex].toString() + " "
-                    }
-                    if(shortLine.length == 0) continue
-                    safe = CheckLineSafety(shortLine)
-                    println("   Check $shortLine: $safe (${shortLine.length})")
-                    if(safe)
-                    {
-                        break
-                    }
-                }
+                safe = checkWithDampener(currentLine, safe)
             }
             if(safe)
                 numberOfSafeReports++
             println("Line $currentLine: $safe")
         }
-
         return numberOfSafeReports.toString()
     }
 
+    private fun checkWithDampener(currentLine: String, safe: Boolean): Boolean {
+        var safe1 = safe
+        val levelArray: Array<Int> = currentLine.split("\\s+".toRegex()).map { it.toInt() }.toTypedArray()
+        for (i in levelArray.indices) {
+            var shortLine: String = ""
+            for (levelIndex in levelArray.indices) {
+                if (i == levelIndex) continue
+                shortLine += levelArray[levelIndex].toString() + " "
+            }
+            if (shortLine.isEmpty()) continue
+            safe1 = checkLineSafety(shortLine)
+            println("   Check $shortLine: $safe1 (${shortLine.length})")
+            if (safe1) {
+                break
+            }
+        }
+        return safe1
+    }
 }

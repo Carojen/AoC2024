1aabfb5c2706c9be12ab88961cb3f63bb147873d
Jenny <jenny.c.soderberg@gmail.com>
2024-12-15 14:03:26 +0100

    Day 2 part 2

--- AdventOfCode/src/Main.kt
+++ AdventOfCode/src/Main.kt
@@ -16,8 +16,10 @@ fun main() {
         result = Day1b(input)
     else if(dayAndPart == "2 a")
         result = Day2a(input)
-    println("The result of $dayAndPart is ${result}")
+    else if(dayAndPart == "2 b")
+        result = Day2b(input)
 
+    println("The result of $dayAndPart is ${result}")
     println(result)
 }
 
@@ -33,52 +35,86 @@ fun SplitLines(input: String): Array<String>
     return lines
 }
 
-fun Day2a(input: String):String
+fun Day2b(input: String): String
+{
+    return Day2(input, true)
+}
+
+fun Day2a(input: String): String
+{
+    return Day2(input, false)
+}
+
+fun CheckLineSafety(line: String): Boolean
+{
+    var currentLine = line.replace(Regex("\\s+"), " ")
+    val levels = currentLine.split(" ")
+    var previousLevel: Int? = null
+    var safe: Boolean = true
+    for(i in levels.indices)
+    {
+        val currentLevel = levels[i].trim().toIntOrNull()
+        if(currentLevel == null)
+            continue
+
+        if(previousLevel == null)
+        {
+            previousLevel = currentLevel
+            continue
+        }
+
+        if(currentLevel == previousLevel)
+        {
+            safe = false
+        }
+        else if(abs(previousLevel - currentLevel) < 1 || abs(previousLevel - currentLevel) > 3)
+        {
+            safe = false
+        }
+        else if(i > 1 && (currentLevel-previousLevel).sign != (previousLevel - levels[i-2].toInt()).sign)
+        {
+            safe = false
+        }
+        previousLevel = currentLevel
+        if(!safe)
+            break
+    }
+    return safe
+}
+
+fun Day2(input: String, allowOneOff: Boolean = false):String
 {
     val lines = SplitLines(input)
     var numberOfSafeReports = 0
     for(line in lines)
     {
-        //println("Line $line")
-        val levels = line.split(" ")
-        var previousLevel: Int = 0
-        var safe: Boolean = true
-        for(i in levels.indices)
-        {
-            if(i == 0)
-            {
-                previousLevel = levels[i].toInt()
-                continue
-            }
-            //println(levels[i])
-            if(levels[i].trim().toIntOrNull() == null)
-                continue
+        if(line.trim().length == 0) continue
 
-            val currentLevel = levels[i].trim().toInt()
-            //println(previousLevel.toString() + " " + currentLevel.toString())
-            if(currentLevel == previousLevel)
+        val currentLine = line.trim()
+        var safe = CheckLineSafety(currentLine)
+        if(!safe && allowOneOff)
+        {
+            val levelArray: Array<Int> = currentLine.split("\\s+".toRegex()).map{it.toInt()}.toTypedArray()
+            for(i in levelArray.indices)
             {
-                safe = false
-                //println("Same")
-                break
+                var shortLine: String = ""
+                for(levelIndex in levelArray.indices)
+                {
+                    if(i == levelIndex) continue
+                    shortLine += levelArray[levelIndex].toString() + " "
+                }
+                if(shortLine.length == 0) continue
+                safe = CheckLineSafety(shortLine)
+                println("   Check $shortLine: $safe (${shortLine.length})")
+                if(safe)
+                {
+                    break
+                }
             }
-            else if(abs(previousLevel - currentLevel) < 1 || abs(previousLevel - currentLevel) > 3)
-            {
-                safe = false
-                //println("Too big difference")
-            }
-            else if(i > 1 && (currentLevel-previousLevel).sign != (previousLevel - levels[i-2].toInt()).sign)
-            {
-                safe = false
-                //println("Different signs")
-            }
-            previousLevel = currentLevel
-            if(!safe)
-                break
         }
         if(safe)
             numberOfSafeReports++
-        //println(safe)
+        println("Line $currentLine: $safe")
     }
 
     return numberOfSafeReports.toString()

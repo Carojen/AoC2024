2db092a9ff32f17463af2837fc46008feb3e4976
Jenny <jenny.c.soderberg@gmail.com>
2024-12-15 16:06:56 +0100

    Day 3 part 2

--- AdventOfCode/src/Day3.kt
+++ AdventOfCode/src/Day3.kt
@@ -1,27 +1,56 @@
 class Day3:Day() {
     override fun RunPartOne(input: String): String
     {
-        return FindAndCalculateMultipliers(input).toString()
+        return FindAndCalculateMultipliers(input, useEnablers = false).toString()
     }
 
     override fun RunPartTwo(input: String): String {
-        TODO("Not yet implemented")
+        return FindAndCalculateMultipliers(input, useEnablers = true).toString()
     }
 
-    private fun FindAndCalculateMultipliers(input: String): Int
+    private fun FindAndCalculateMultipliers(input: String, useEnablers: Boolean = false): Int
     {
         var totalAmount:Int = 0
         val instructions = input.split("mul")
+
+        var previousSection: String? = null
+        var enabled: Boolean = true
         for(section in instructions)
         {
             val numbers = section.split(")")[0].removePrefix("(").split(",")
-            if(numbers.count() < 2) continue
+            println("Check $numbers")
+            if(previousSection != null && useEnablers)
+            {
+                println("Previous section: $previousSection")
+                val dos = previousSection.split("do()")
+                if(dos.size > 1)
+                {
+                    enabled = true
+                    println("Found do()")
+                }
+                if(dos.last().contains("don't()"))
+                {
+                    enabled = false
+                    println("Found later don't()")
+                }
+            }
+            if(!enabled)
+            {
+                previousSection = section
+                continue
+            }
+            if(numbers.count() < 2)
+            {
+                previousSection = section
+                continue
+            }
 
             if(numbers[0].toIntOrNull() != null && numbers[1].toIntOrNull() != null )
             {
                 val product = numbers[0].toInt() * numbers[1].toInt()
                 totalAmount += product
             }
+            previousSection = section
         }
         return totalAmount
     }

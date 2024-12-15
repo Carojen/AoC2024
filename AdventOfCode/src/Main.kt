a4d38e87dc01a74a5b58dc35fa55572803c16eb6
Jenny <jenny.c.soderberg@gmail.com>
2024-12-15 12:24:08 +0100

    Day 2 part 1

--- AdventOfCode/src/Main.kt
+++ AdventOfCode/src/Main.kt
@@ -1,9 +1,9 @@
 import java.io.File
 import kotlin.math.abs
+import kotlin.math.sign
 
 
 fun main() {
-    val name = "Kotlin"
   println("Which day do you want to run?")
     val dayAndPart = readln()
     val day = dayAndPart.split(" ")[0]
@@ -11,31 +11,84 @@ fun main() {
     var result = "NaN"
 
     if(dayAndPart == "1 a")
-    {
         result = Day1a(input)
-    }
     else if(dayAndPart == "1 b")
-    {
         result = Day1b(input)
-    }
+    else if(dayAndPart == "2 a")
+        result = Day2a(input)
     println("The result of $dayAndPart is ${result}")
 
     println(result)
 }
 
-fun ReadFile(day: String):String
+fun ReadFile(day: String): String
 {
     println("Read file input: " + day)
     val input = File("input/"+day+".txt").inputStream().readBytes().toString(Charsets.UTF_8)
     return input
 }
+fun SplitLines(input: String): Array<String>
+{
+    val lines = input.split("\n").toTypedArray()
+    return lines
+}
 
+fun Day2a(input: String):String
+{
+    val lines = SplitLines(input)
+    var numberOfSafeReports = 0
+    for(line in lines)
+    {
+        //println("Line $line")
+        val levels = line.split(" ")
+        var previousLevel: Int = 0
+        var safe: Boolean = true
+        for(i in levels.indices)
+        {
+            if(i == 0)
+            {
+                previousLevel = levels[i].toInt()
+                continue
+            }
+            //println(levels[i])
+            if(levels[i].trim().toIntOrNull() == null)
+                continue
+
+            val currentLevel = levels[i].trim().toInt()
+            //println(previousLevel.toString() + " " + currentLevel.toString())
+            if(currentLevel == previousLevel)
+            {
+                safe = false
+                //println("Same")
+                break
+            }
+            else if(abs(previousLevel - currentLevel) < 1 || abs(previousLevel - currentLevel) > 3)
+            {
+                safe = false
+                //println("Too big difference")
+            }
+            else if(i > 1 && (currentLevel-previousLevel).sign != (previousLevel - levels[i-2].toInt()).sign)
+            {
+                safe = false
+                //println("Different signs")
+            }
+            previousLevel = currentLevel
+            if(!safe)
+                break
+        }
+        if(safe)
+            numberOfSafeReports++
+        //println(safe)
+    }
+
+    return numberOfSafeReports.toString()
+}
 
 fun Day1a(input: String):String
 {
-    var firstList: MutableList<Int> = mutableListOf()
-    var secondList: MutableList<Int> = mutableListOf()
-    val lines = input.split("\n").toTypedArray()
+    val firstList: MutableList<Int> = mutableListOf()
+    val secondList: MutableList<Int> = mutableListOf()
+    val lines = SplitLines(input)
     for(line in lines)
     {
         val values = line.split("\\s+ ".toRegex()).toTypedArray()

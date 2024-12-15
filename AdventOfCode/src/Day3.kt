c9fee42e0a7a8482c760a6a8033e2dc412efcc64
Jenny <jenny.c.soderberg@gmail.com>
2024-12-15 15:12:12 +0100

    Day 3 part 1

--- AdventOfCode/src/Day3.kt
+++ AdventOfCode/src/Day3.kt
@@ -1,9 +1,28 @@
 class Day3:Day() {
-    override fun RunPartOne(input: String): String {
-        TODO("Not yet implemented")
+    override fun RunPartOne(input: String): String
+    {
+        return FindAndCalculateMultipliers(input).toString()
     }
 
     override fun RunPartTwo(input: String): String {
         TODO("Not yet implemented")
+    }
+
+    private fun FindAndCalculateMultipliers(input: String): Int
+    {
+        var totalAmount:Int = 0
+        val instructions = input.split("mul")
+        for(section in instructions)
+        {
+            val numbers = section.split(")")[0].removePrefix("(").split(",")
+            if(numbers.count() < 2) continue
+
+            if(numbers[0].toIntOrNull() != null && numbers[1].toIntOrNull() != null )
+            {
+                val product = numbers[0].toInt() * numbers[1].toInt()
+                totalAmount += product
+            }
+        }
+        return totalAmount
     }
 }
